b6b6f2881af70278cb964679dad2b8546e2270f4
Robert Cloud <rcloud@gmail.com>
2011-10-01 16:20:47 -0500

    moved more modules into jac_2d.pyx
    am having difficulties with Mark's suggestions about invoking and
    discarding the GIL.  I'm not sure how this works.

--- source code/Parallel Cython/cython_parallel_driver.py
+++ source code/Parallel Cython/cython_parallel_driver.py
@@ -9,29 +9,14 @@ def initialize(u):
     u[:,u.shape[0] - 1] = 50 #right column
 
 
-def convergence_test(u_new, u):
-    eps = .0001    
-    cont = False
-    if(np.max(u_new - u) > eps):
-        cont = True
-    return cont
 
-def solver(dim):
+
+
+def setup(dim):
     u = np.zeros((dim, dim), dtype=np.float32)
     initialize(u)
     u_new = np.copy(u)
-
-    cont = True
-    iteration = 0
-    while cont == True:
-        if iteration % 2 == 0: #even
-            jac_2d.solve(u, u_new, dim, dim)
-        else:
-            jac_2d.solve(u_new, u, dim, dim)
-        #test for convergence
-        if iteration % 200 == 0:
-            cont = convergence_test(u_new, u) 
-        iteration = iteration + 1    
+    u_new = jac_2d.solver(dim, u, u_new)
     return u_new
 
 if __name__ == "__main__":
@@ -40,7 +25,7 @@ if __name__ == "__main__":
         sys.exit()
     dim = int(sys.argv[1])
     start = time.time()
-    output = solver(dim)
+    output = setup(dim)
     finish = time.time()
     print finish - start
     fd = open("data.txt", "w")
--- source code/Parallel Cython/jac_2d.pyx
+++ source code/Parallel Cython/jac_2d.pyx
@@ -2,14 +2,38 @@ cimport numpy as np
 cimport cython
 from cython.parallel import *
 ctypedef np.float32_t dtype_t
+import numpy
+
+
+def convergence_test(np.ndarray[dtype_t, ndim=2] u_new, np.ndarray[dtype_t, ndim=2] u):
+    cdef double eps = .0001    
+    cdef int cont = 0
+    if(numpy.max(u_new - u) > eps):
+        cont = 1
+    return cont
+
+
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def solve(np.ndarray[dtype_t, ndim=2] u, 
-          np.ndarray[dtype_t, ndim=2] u_new, 
-          int dimy, int dimx):
-    cdef int y, x
-    for y in prange(1, dimy - 1, nogil=True):
-        for x in range(1, dimx - 1):
-            u_new[y,x] = (u[y + 1,x] + u[y - 1,x] + u[y,x + 1] + u[y,x - 1]) / 4
+           np.ndarray[dtype_t, ndim=2] u_new, 
+           int dimy, int dimx):
+        cdef int y, x              
+        for y in prange(1, dimy - 1, nogil=True):
+            for x in range(1, dimx - 1):
+                u_new[y,x] = (u[y + 1,x] + u[y - 1,x] + u[y,x + 1] + u[y,x - 1]) / 4
             
     
+def solver(int dim, np.ndarray[dtype_t, ndim=2] u, np.ndarray[dtype_t, ndim=2] u_new):
+    cdef int iteration = 0
+    cdef int cont = 1
+    while cont == 1:
+        if iteration % 2 == 0: #even
+            solve(u, u_new, dim, dim)
+        else:
+            solve(u_new, u, dim, dim)
+        #test for convergence            
+        if iteration % 200 == 0:
+            cont = convergence_test(u_new, u) 
+        iteration = iteration + 1    
+    return u_new
